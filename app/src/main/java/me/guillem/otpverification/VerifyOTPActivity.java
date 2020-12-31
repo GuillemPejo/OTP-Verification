b20edc08468835010643606def7968097ed7c855
guillem <guillempejo@gmail.com>
2020-12-31 18:41:34 +0100

    Add first layout setup

---
+++ app/src/main/java/me/guillem/otpverification/VerifyOTPActivity.java
@@ -0,0 +1,14 @@
+package me.guillem.otpverification;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class VerifyOTPActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.verify_send_otp);
+    }
+}
