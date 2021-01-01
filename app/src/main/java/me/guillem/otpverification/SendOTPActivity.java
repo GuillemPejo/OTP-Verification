d070da4b901b523f3403bd162941e648a73fba3f
guillem <guillempejo@gmail.com>
2021-01-01 20:26:28 +0100

    Update logic

--- app/src/main/java/me/guillem/otpverification/SendOTPActivity.java
+++ app/src/main/java/me/guillem/otpverification/SendOTPActivity.java
@@ -2,7 +2,12 @@ package me.guillem.otpverification;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class SendOTPActivity extends AppCompatActivity {
 
@@ -10,5 +15,21 @@ public class SendOTPActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_send_otp);
+
+        final EditText inputMobile = findViewById(R.id.inputMobile);
+        Button buttonGetCode = findViewById(R.id.buttonGetCode);
+
+        buttonGetCode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (inputMobile.getText().toString().trim().isEmpty()){
+                    Toast.makeText(SendOTPActivity.this, "Enter mobile", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                Intent intent = new Intent(getApplicationContext(), VerifyOTPActivity.class);
+                intent.putExtra("mobile", inputMobile.getText().toString());
+                startActivity(intent);
+            }
+        });
     }
 }
